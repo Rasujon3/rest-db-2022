8178b68357b4625f2592e8c6c87510077216c1c9
Rasujon3 <rasujon3@gmail.com>
2022-08-08 20:43:10 +0600

    MongoDB atlas connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
-const PORT = process.env.PORT || 5000;
+const config = require("./config/config");
+const PORT = config.app.port;
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
