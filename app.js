2074dd63f8032fdcac3ec1b8f9b0a4eb70c505a7
Rasujon3 <rasujon3@gmail.com>
2022-08-08 19:44:28 +0600

    invalid route set

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/./views/index.html");
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "route not found",
+  });
+});
+
 module.exports = app;
